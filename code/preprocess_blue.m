e4e84ae25060e4e7d3495b82e66734e5769a397c
Rishabh Biyani <Rishabh Biyani>
2017-04-03 02:54:02 -0400

    Minor equation mistake in normalization fixed

--- code/preprocess_blue.m
+++ code/preprocess_blue.m
@@ -3,6 +3,6 @@ im_d = im2double(im);
 im_c = imadjust(im_d, stretchlim(im_d));
 im_b = im_c(:,:,3);
 im_b = medfilt2(im_b, [3 3]); 
-im_b = (im_b - im_c(:,:,1)) ./ (im_c(:,:,1) + im_c(:,:,2) + im_c(:,:,3));
+im_b = max(0,(im_b - im_c(:,:,1)) ./ (im_c(:,:,1) + im_c(:,:,2) + im_c(:,:,3)));
 im_b_usig = im2uint8(im_b);
 end
