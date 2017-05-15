0bdf65a6a30d6d9e729dd5ab74fa0c7acf4a9526
rishabh1b <biyanirishabh@gmail.com>
2017-05-15 15:31:02 -0400

    Visualization complete

--- CreateVideo.m
+++ CreateVideo.m
@@ -5,17 +5,20 @@ outputVideo = VideoWriter(fullfile(outputfolder,filename),'MPEG-4');
 outputVideo.FrameRate = 30;
 
 open(outputVideo)
-filePattern = sprintf('%s/*.png', datafolder);
-baseFileNames = dir(filePattern);
-curr_file_names = {baseFileNames.name};
-numberOfImageFiles = length(baseFileNames);
-for k = 1:numberOfImageFiles
-    fullfilename = fullfile(datafolder,cell2mat(curr_file_names(k)));
+%filePattern = sprintf('%s/*.jpg', datafolder);
+D = dir([datafolder,'\*.jpg']);
+%baseFileNames = dir(filePattern);
+%curr_file_names = {baseFileNames.name};
+%numberOfImageFiles = length(baseFileNames);
+numOfFrames = length(D);
+for k = 1:numOfFrames
+    curr_file = sprintf('im_vo_%d.jpg',k);
+    fullfilename = fullfile(datafolder,curr_file);
     if(~exist(fullfilename,'file'))
        continue;
     end
     im = imread(fullfilename);
-    im = demosaic(im,'gbrg');
+    %im = demosaic(im,'gbrg');
     writeVideo(outputVideo,im)
 end
 close(outputVideo)
